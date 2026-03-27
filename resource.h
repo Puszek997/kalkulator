32c6a8695ffb99179cf8c2673eecb6204b1f2c1f
Mikołaj Gołyński <124436127+Puszek997@users.noreply.github.com>
2026-03-27 14:11:11 +0100

    cos

---
+++ resource.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define ID_MAIN_MENU         101
+#define ID_ACCELERATORS      102
+#define ID_ICON              103
+
+#define ID_FILE_EXIT         40001
+
+#define ID_EDIT_COPY         40002
+#define ID_EDIT_PASTE        40003
+#define ID_EDIT_CLEAR        40004
+
+#define ID_MODE_BASIC        40005
+#define ID_MODE_PROGRAMMER   40006
+
+#define ID_VIEW_HEX          40007
+#define ID_VIEW_DEC          40008
+#define ID_VIEW_OCT          40009
+#define ID_VIEW_BIN          40010
+#define ID_VIEW_SEPARATORS   40011
+#define ID_VIEW_ALWAYSONTOP  40012
+
+#define ID_HELP_ABOUT        40013
